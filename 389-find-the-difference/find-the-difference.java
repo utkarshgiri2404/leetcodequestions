519bc95a0438c7379a1adc5fb33f91af9af19810
utkarshgiri2404 <utk.giri24@gmail.com>
2026-08-27 21:06:30 +0530

    Time: 2 ms (66.24%) | Memory: 43 MB (84.98%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int a=0,b=0;
+        for(int i=0;i<s.length();i++){
+            a+=s.charAt(i);
+        }
+        for(int i=0;i<t.length();i++){
+            b+=t.charAt(i);
+        }
+        return (char) (b-a);
+    }
+}
