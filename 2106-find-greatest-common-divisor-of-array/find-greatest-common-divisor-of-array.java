32d0e68638f305a273fe3e0946fb9b3ffdc7d709
utkarshgiri2404 <utk.giri24@gmail.com>
2026-08-28 21:41:42 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (26.32%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        int min=nums[0];
+        int max=nums[0];
+        for(int n:nums){
+            if(n<min) min=n;
+            if(n>max) max=n;
+        }
+        int gcd=1;
+        for(int i=1;i<=min;i++){
+            if(min%i==0 && max%i==0){
+                gcd=i;
+            }
+        }
+        return gcd;
+    }
+}
